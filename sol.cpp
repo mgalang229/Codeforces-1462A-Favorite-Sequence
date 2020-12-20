f650a27c88b8d634c15bb053003eb41844372920
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-12-20 21:37:12 +0800

    Update sol.cpp

--- sol.cpp
+++ sol.cpp
@@ -11,7 +11,7 @@ void solve() {
 	for(int i=0; i<n; ++i)
 		cin >> a[i];
 	map<int, int> mp;
-	//store a[i] in map with key of odd numbers from n to n/2+1(increment)
+	//store a[i] in map with key of odd numbers from n to n/2(increment)
 	int cnt=1;
 	for(int i=0; i<=n/2; ++i) {
 		mp[cnt]=a[i];
